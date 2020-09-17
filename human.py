b2d234fdd79d6be0892ad7145b69d6f8c6539521
Peter Lin <plin11@binghamton.edu>
2020-09-16 23:54:30 -0400

    first commit

---
+++ human.py
@@ -0,0 +1,9 @@
+class human:
+    species = "homosapien"
+
+def __intit__(self, name, age):
+    self.name = name
+    self.age = age
+
+def speak(self, sound):
+    return f"{self.name} says {sound}"
---
+++ run.py
@@ -0,0 +1,4 @@
+from human.human import human
+danny = human("Danny", 18)
+danny.speak("TRASH ALL DAY")
+danny.speak("YOU SUCK!")
